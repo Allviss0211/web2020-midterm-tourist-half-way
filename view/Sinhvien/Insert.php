21e4af01194a6a3030a6c24e703b162927510046
TienDat <drdat1415@gmail.com>
2020-07-04 10:33:42 +0700

    edit view sinhvien

--- view/Sinhvien/Insert.php
+++ view/Sinhvien/Insert.php
@@ -1,21 +1,31 @@
 <!-- Article --><div class="article">
-<h2><span><a href="admin.php?mod=sinhvien&act=insert">Thêm Sinh vien</a></span></h2>
+<h2><span><a href="admin.php?mod=sinhvien&act=insert">Thêm sinh viên</a></span></h2>
   <p>
-  	<form class="form" method="post" action="admin.php?mod=sinhvien&act=insert" enctype="multipart/form-data">
-        <p><label>Mã sinh viên (*)</label><input type="text" name="txtMaSV" id="txtMaSV" /></p>
-        <p><label>Tên sinh viên (*)</label><input type="text" name="txtHoten" id="txtHoten" /></p>
-        <p><label>Ngày sinh </label><input type="date" name="txtNgaysinh" id="txtNgaysinh" /></p>
-        <p><label>Email </label><input type="email" name="txtEmail" id="txtEmail" /></p>
-        <select name="sllop">
-        	<option value="0">Chọn Lớp</option>
+  	<form class="form" method="post" action="admin.php?mod=sinvien&act=insert" enctype="multipart/form-data">
+        <p><label>Tên sinh viên (*)</label><input type="text" name="txtTenSV" id="txtTenSV" /></p>
+        <p><label>Khoa (*)</label>
+        <select name="slKhoa">
+        	<option value="0">Chọn khoa</option>
             <?php
-				foreach($cate as $row)
+				foreach($khoa as $row)
 				{
-					echo "<option value=\"$row[MaLop]\" >$row[TenLop]</option>";
+					echo "<option value=\"$row[MaKhoa]\" >$row[MaKhoa]</option>";
 				}
 			?>
         </select></p>
+        
+        <p><label>Lớp (*)</label>
+        <select name="slLop">
+        	<option value="0">Chọn lớp</option>
+            <?php
+				foreach($lop as $row)
+				{
+					echo "<option value=\"$row[MaLop]\" >$row[MaLop]</option>";
+				}
+			?>
+        </select></p>   
         <p><label>&nbsp;</label><input type="submit" value="Lưu" name="btnSave" id="btnSave" /></p>
+        
         <p id="error" class="error"></p>
     </form>
   </p>
