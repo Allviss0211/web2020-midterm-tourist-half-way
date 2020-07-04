d38e3d5beea7ca7f9127204ebe5e5f405391faea
kimchi98 <63057791+kimchi98@users.noreply.github.com>
2020-07-04 10:29:09 +0700

    Update Sinh vien view

--- view/Sinhvien/Insert.php
+++ view/Sinhvien/Insert.php
@@ -1,31 +1,21 @@
 <!-- Article --><div class="article">
-<h2><span><a href="admin.php?mod=sinhvien&act=insert">Thêm sinh viên</a></span></h2>
+<h2><span><a href="admin.php?mod=sinhvien&act=insert">Thêm Sinh vien</a></span></h2>
   <p>
-  	<form class="form" method="post" action="admin.php?mod=sinvien&act=insert" enctype="multipart/form-data">
-        <p><label>Tên sinh viên (*)</label><input type="text" name="txtTenSV" id="txtTenSV" /></p>
-        <p><label>Khoa (*)</label>
-        <select name="slKhoa">
-        	<option value="0">Chọn khoa</option>
+  	<form class="form" method="post" action="admin.php?mod=sinhvien&act=insert" enctype="multipart/form-data">
+        <p><label>Mã sinh viên (*)</label><input type="text" name="txtMaSV" id="txtMaSV" /></p>
+        <p><label>Tên sinh viên (*)</label><input type="text" name="txtHoten" id="txtHoten" /></p>
+        <p><label>Ngày sinh </label><input type="date" name="txtNgaysinh" id="txtNgaysinh" /></p>
+        <p><label>Email </label><input type="email" name="txtEmail" id="txtEmail" /></p>
+        <select name="sllop">
+        	<option value="0">Chọn Lớp</option>
             <?php
-				foreach($khoa as $row)
+				foreach($cate as $row)
 				{
-					echo "<option value=\"$row[MaKhoa]\" >$row[MaKhoa]</option>";
+					echo "<option value=\"$row[MaLop]\" >$row[TenLop]</option>";
 				}
 			?>
         </select></p>
-        
-        <p><label>Lớp (*)</label>
-        <select name="slLop">
-        	<option value="0">Chọn lớp</option>
-            <?php
-				foreach($lop as $row)
-				{
-					echo "<option value=\"$row[MaLop]\" >$row[MaLop]</option>";
-				}
-			?>
-        </select></p>   
         <p><label>&nbsp;</label><input type="submit" value="Lưu" name="btnSave" id="btnSave" /></p>
-        
         <p id="error" class="error"></p>
     </form>
   </p>
